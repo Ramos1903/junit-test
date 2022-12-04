9312cb8a6a614027f120733d158c7d8270c54373
Vinicius <vinicius.jr08@hotmail.com>
2022-12-04 12:59:14 -0300

    feat: added calculator class to addTwoNumbers

--- src/main/java/junit/test/junit_test/App.java
+++ src/main/java/junit/test/junit_test/App.java
@@ -1,13 +1,8 @@
 package junit.test.junit_test;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+    	Calculator calculator = new Calculator();
+        System.out.println(calculator.add(1, 2));
     }
 }
---
+++ src/main/java/junit/test/junit_test/Calculator.java
@@ -0,0 +1,7 @@
+package junit.test.junit_test;
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+}
